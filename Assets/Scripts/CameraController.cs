9db67c21af5faa87abb46d9f8c9b90fab9f5ce6e
Agustin-Venegas <68612141+Agustin-Venegas@users.noreply.github.com>
2025-06-10 17:44:57 -0400

    Inputs
    inputs con sistema nuevo de inputs
    Tambien, scripts para mover camara, cambiar animaciones dependiendo de a que lado va, etc
    Backups de prefabs de kayak

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class CameraController : MonoBehaviour
+{
+
+    Quaternion rot;
+    public GameObject Camera;
+
+    public float Sensitivity = 1f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rot = Camera.transform.rotation;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+
+        Camera.transform.rotation = rot;
+    }
+
+    //recibe evento de player input, mouse o stick derecho
+    public void OnLook(InputValue v)
+    {
+        Vector2 delta = Sensitivity * v.Get<Vector2>();
+
+        Vector3 euler = rot.eulerAngles;
+
+        //clampear entre 60 y 0 y 360 y 300
+        euler.x = euler.x - delta.y > 60 && euler.x - delta.y < 300 ? euler.x : euler.x - delta.y;
+        euler.y = euler.y + delta.x;
+        euler.z = 0;
+
+        //Mathf.Clamp(rot.eulerAngles.x - delta.y,-70f,25f)
+
+        Debug.Log("Rot:"+rot.eulerAngles.x);
+
+        rot = Quaternion.Euler(euler);
+    }
+}
--- Assets/Scripts/PaddleController.cs
+++ Assets/Scripts/PaddleController.cs
@@ -1,18 +1,24 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 using UnityEngine.InputSystem.XR;
 using UnityEngine.XR;
 
-//activa y desactiva los paddles dependiendo si estan conectados o no
+//Controlar remo con mando xbox o PC, con animaciones
+//antes, servia para apagar y prender remo segun mano vr conectada
 public class PaddleController : MonoBehaviour 
 {
-    
     public GameObject rightpaddle;
     public GameObject leftpaddle;
 
+    public Animator animator;
+
+    Vector2 input;
+
 	void Start ()
     {
+        /*
         if (XRController.leftHand == null)
         {
             rightpaddle.SetActive(true);
@@ -22,12 +28,52 @@ public class PaddleController : MonoBehaviour
         {
             leftpaddle.SetActive(true);
         }
+        */
 
     }
 
+    public void OnMove(InputValue movementValue)
+    {
+        input = movementValue.Get<Vector2>();
+    }
+
+    private void FixedUpdate()
+    {
+        float r = Mathf.Clamp01(input.x);
+        float l = Mathf.Clamp01(-input.x);
+        float f = input.y;
+
+        animator.SetFloat("Forward", f);
+        animator.SetFloat("Right", r);
+        animator.SetFloat("Left", l);
+
+        if (f == 0)
+        {
+            animator.SetFloat("ActualSpeed", 0f);
+        } else
+        {
+            animator.SetFloat("ActualSpeed", 1f);
+        }
+
+        /*
+        if (input.y < 0)
+        {
+            
+            animator.SetFloat("ActualSpeed", 1f);
+        } else if (input.y == 0)
+        {
+            animator.SetFloat("Forward", 0f);
+        } else
+        {
+            animator.SetFloat("Forward", f);
+        }
+        */
+    }
+
+
     void Update ()
     {
-        if (XRController.leftHand == null)
+        /*if (XRController.leftHand == null)
         {
             rightpaddle.SetActive(true);
         }
@@ -36,6 +82,6 @@ public class PaddleController : MonoBehaviour
         {
             leftpaddle.SetActive(true);
         }
-        
+        */
 	}    
 }
